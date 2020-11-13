68191e973b96c2e6234f95e7d203e29a6b940267
Serena Hasler <srhasler@yahoo.com>
2020-11-12 20:55:16 -0600

    Add setTimeoutAsync then/catch/finally

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import got from "got";
+// own version of built in callback could do aSetTimeout
+// more accurate name would be setTimeoutPromise
+// this is a function factory pattern
+const setTimeoutAsync = (fxn, delay) =>
+  new Promise((resolve) => {
+    // this behaves like a 'return' the 'reject' is never triggered
+    setTimeout(() => {
+      resolve(fxn());
+    }, delay);
+  });
 
-got("https://www.cnn.com")
-  .then((res) => {
-    console.log(res);
+// need to understand this part -- use .then to get to results; .catch to grab errors and can use .finally
+setTimeoutAsync(() => {
+  return "hi";
+}, 2000)
+  .then((results) => {
+    console.log(results, "done from promise!");
   })
-  .catch((err) => {
-    console.error(err.message);
+  .catch((error) => {
+    console.error("fake error", error);
   })
   .finally(() => {
-    console.info("Class is over!");
+    console.log("no matter what we are done!");
   });
