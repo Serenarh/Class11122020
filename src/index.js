64c724eb93255d2ae429ae30fdef38f1ea525edb
Serena Hasler <srhasler@yahoo.com>
2020-11-12 20:24:50 -0600

    Add then catch finally

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import https from "http";
+// own version of built in callback could do aSetTimeout
+// more accurate name would be setTimeoutPromise
+// this is a function factory pattern
+const setTimeoutAsync = (fxn, delay) =>
+  new Promise((resolve) => {
+    // this behaves like a 'return' the 'reject' is never triggered
+    setTimeout(() => {
+      resolve(fxn());
+    }, delay);
+  });
 
-const interval = setInterval(() => {
-  console.log("hi");
-}, 200);
-
-setTimeout(() => {
-  clearInterval(interval);
-}, 2000);
+// need to understand this part -- use .then to get to results; .catch to grab errors and can use .finally
+setTimeoutAsync(() => {
+  return "hi";
+}, 2000)
+  .then((results) => {
+    console.log(results, "done from promise!");
+  })
+  .catch((error) => {
+    console.error("fake error", error);
+  })
+  .finally(() => {
+    console.log("no matter what we are done!");
+  });
