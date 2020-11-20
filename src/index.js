98c349fff5296529814fbb181757a6ee019bea1e
Serena Hasler <srhasler@yahoo.com>
2020-11-19 19:49:37 -0600

    Add try/catch

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import { info } from "console";
 import { promises as fs } from "fs";
 import got from "got";
 
 (async () => {
-  const filmsRes = await got("https://ghibliapi.herokuapp.com/films").json();
-  fs.writeFile("test.txt", filmsRes.toString())
-    .then(() => {
-      console.log("fin!");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  try {
+    const filmsRes = await got("https://ghibliapi.herokuapp.com/films").json();
+
+    fs.writeFile("test.txt", filmsRes.toString())
+      .then(() => {
+        console.log("fin!");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } catch (error) {
+    console.error(error);
+  }
 })();
 
 got("https://ghibliapi.herokuapp.com/films")
